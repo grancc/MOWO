ab5d341e86aa1f41c01892a8cabcd39ea8b0daa1
unknown <herrytr@mail.ru>
2025-04-08 15:00:29 +1000

    fix v4 promo text, number count

--- static/js/numbercount.js
+++ static/js/numbercount.js
@@ -1,7 +1,8 @@
 let valueDisplays = document.querySelectorAll(".number-num span");
-let interval = 4000;
+let interval = 2500;
 
-valueDisplays.forEach((valueDisplay) => {
+// Функция для запуска анимации
+function startCounter(valueDisplay) {
   let startValue = 0;
   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
   let duration = Math.floor(interval / endValue);
@@ -12,4 +13,20 @@ valueDisplays.forEach((valueDisplay) => {
       clearInterval(counter);
     }
   }, duration);
+}
+
+// Создаем Observer, который будет отслеживать, когда блок попадет в область видимости
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Если блок в области видимости, запускаем анимацию
+      startCounter(entry.target);
+      observer.unobserve(entry.target); // Останавливаем отслеживание этого элемента
+    }
+  });
+}, { threshold: 0.5 }); // threshold определяет, насколько должен быть виден элемент (в данном случае 50%)
+
+valueDisplays.forEach((valueDisplay) => {
+  // Добавляем каждый элемент в observer для отслеживания
+  observer.observe(valueDisplay);
 });
