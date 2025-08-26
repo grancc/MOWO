fc8ef3090b82d5c39aecd96a85310ef0e8015e29
Gribochek Grant <herrytr@mail.ru>
2025-08-26 16:11:58 +1000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-echo "Происходит минификация CSS-файлов..."
-for file in /usr/src/app/static/css/*.css; do
-    uglifycss "$file" > temp.css
-    mv temp.css "$file"
-    echo "Файл $file минифицирован"
-done
+# echo "Происходит минификация CSS-файлов..."
+# for file in /usr/src/app/static/css/*.css; do
+#     uglifycss "$file" > temp.css
+#     mv temp.css "$file"
+#     echo "Файл $file минифицирован"
+# done
 
-echo "Происходит минификация JS-файлов..."
-for file in /usr/src/app/static/js/*.js; do
-    uglifyjs "$file" --compress --mangle -o "$file"
-    echo "Файл $file минифицирован"
-done
+# echo "Происходит минификация JS-файлов..."
+# for file in /usr/src/app/static/js/*.js; do
+#     uglifyjs "$file" --compress --mangle -o "$file"
+#     echo "Файл $file минифицирован"
+# done
 
 python manage.py runserver 0.0.0.0:8000
 
